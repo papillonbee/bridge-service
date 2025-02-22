96eba921c58b751698a6fad0fa06d8b2230cbb1b
Papan Yongmalwong <papan.yongmalwong@seamoney.com>
2025-02-23 01:41:20 +0800

    cors allow origin injected from environment variable

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
+    allow_origins = [settings.allow_origin],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
